c8ef3692bd001e80e515b50714de032974b3995d
Sanskar Suryavanshi personal <ssuryavanshi0001@gmail.com>
2023-05-01 09:57:51 +0530

    last commit

--- Backend_block/routes/storyRoute.js
+++ Backend_block/routes/storyRoute.js
@@ -1,9 +1,10 @@
 let express = require("express");
 const { getAllStories,createStory, updateStory, deleteStory, getOneStory } = require("../controllers/storyController");
+const { isAuthenticatedUser } = require("../middleware/auth");
 
 let router = express.Router();
 
-router.route("/stories").get(getAllStories);
+router.route("/stories").get(isAuthenticatedUser ,getAllStories);
 router.route("/stories/new").post(createStory); 
 router.route("/stories/:id").put(updateStory).delete(deleteStory).get(getOneStory)
 
